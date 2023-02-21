c476f19131bd4abf4dd5aeaa5503eacfaf21c2e1
ShenLong <93514880+LongDang777@users.noreply.github.com>
2023-02-21 09:56:52 +0700

    add delay 3000ms

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults({delay: 3000})
 
 server.use(middlewares)
 // Add this before server.use(router)
